752c7612492137210dcea062fb1365889d563bac
Leo Mdivani <leomdiv@gmail.com>
2014-12-28 21:58:03 +0000

    night

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -2,6 +2,8 @@ window.smoothScroll = function(link) {
   var target = $(link);
   var body = $('body');
   var scrollTo = $(target.attr('href')).offset().top;
+  $('#navbarLinks').removeClass("in");
+  
   if ($('header').css('position') === 'fixed'){
     scrollTo -= 51;
   }
